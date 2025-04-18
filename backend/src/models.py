a3c6d3f960974536844ba85cccb5392aa3174048
Morswin <41349347+Morswin@users.noreply.github.com>
2025-04-19 01:39:28 +0200

    Update models.py
    Relations almost finished. It's too late hour for me to finish

--- backend/src/models.py
+++ backend/src/models.py
@@ -3,31 +3,6 @@ from enum import Enum
 from sqlmodel import Field, SQLModel, Relationship
 
 
-class TicketType(Enum):
-    COMPLAIN = 1
-    FOOD_NOT_DELIVERED = 2
-    FOOD_IS_COLD = 3
-
-
-class TicketState(Enum):
-    PENDING = 1
-    RESOLVED = 2
-    REJECTED = 3
-
-
-class Ticket(SQLModel, table=True):
-    __tablename__ = "ticket"
-
-    # Keys
-    id_ticket: int | None = Field(default=None, primary_key=True)
-    id_user: int | None = Field(default=None, foreign_key="user.id_user")
-
-    # Fields
-    ticket_type: TicketType
-    ticket_state: TicketState
-    description: str
-
-
 # It must be declared befor user and order for some reason. Otherwise the IDE spams with errors.
 class OrderHistory(SQLModel, table=True):
     __tablename__ = "order_history"
@@ -66,7 +41,8 @@ class User(SQLModel, table=True):
     id_user: int | None = Field(default=None, primary_key=True)
 
     # Relations
-    orders: list["Order"] = Relationship(back_populate="users", link_model=OrderHistory)
+    orders: list["Order"] = Relationship(back_populates="users", link_model=OrderHistory)
+    tickets: list["Ticket"] = Relationship(back_populates="user")
 
     # Fields
     user_type: UserType
@@ -76,6 +52,69 @@ class User(SQLModel, table=True):
     email: str
     gender: Gender
     pay: float
+
+
+class TicketType(Enum):
+    COMPLAIN = 1
+    FOOD_NOT_DELIVERED = 2
+    FOOD_IS_COLD = 3
+
+
+class TicketState(Enum):
+    PENDING = 1
+    RESOLVED = 2
+    REJECTED = 3
+
+
+class Ticket(SQLModel, table=True):
+    __tablename__ = "ticket"
+
+    # Keys
+    id_ticket: int | None = Field(default=None, primary_key=True)
+
+    # Relations
+    user: User | None = Relationship(back_populates="tickets")
+    orders: list["Order"] = Relationship(back_populates="tickets")
+
+    # Fields
+    ticket_type: TicketType
+    ticket_state: TicketState
+    description: str
+
+
+class Address(SQLModel, table=True):
+    __tablename__ = "address"
+
+    # Keys
+    id_address: int | None = Field(default=None, primary_key=True)
+
+    # Relations
+    orders: list["Order"] = Relationship(back_populates="address")
+
+    # Fields
+    city: str
+    street: str
+    zip_code: str
+    house_number: int
+    apartment_number: int
+
+
+class Restaurant(SQLModel, table=True):
+    __tablename__ = "restaurant"
+
+    # Keys
+    id_restaurant: int | None = Field(default=None, primary_key=True)
+    id_hours: int | None = Field(default=None, foreign_key="hours.id_hours")
+    id_score: int | None = Field(default=None, foreign_key="score.id_score")
+
+    # Relations
+    orders: list["Order"] = Relationship(back_populates="restaurant")
+
+    # Fields
+    name: str
+    pay_cut: float
+    email: str
+    phone: str
 
 
 class OrderStatus(Enum):
@@ -92,12 +131,12 @@ class Order(SQLModel, table=True):
 
     # Keys
     id_order: int | None = Field(default=None, primary_key=True)
-    id_address: int | None = Field(default=None, foreign_key="address.id_address")
-    id_restaurant: int | None = Field(default=None, foreign_key="restaurant.id_restaurant")
-    id_ticket: int | None = Field(default=None, foreign_key="ticket.id_ticket")
 
     # Relations
     users: list["User"] = Relationship(back_populates="orders", link_model=OrderHistory)
+    address: Address | None = Relationship(back_populates="orders")
+    restaurant: Restaurant | None = Relationship(back_populates="orders")
+    ticket: Ticket | None = Relationship(back_populates="orders")
 
     # Fields
     status: OrderStatus
@@ -106,20 +145,6 @@ class Order(SQLModel, table=True):
     weight: float
     score_driver: float
     score_restaurant: float
-
-
-class Address(SQLModel, table=True):
-    __tablename__ = "address"
-
-    # Keys
-    id_address: int | None = Field(default=None, primary_key=True)
-
-    # Fields
-    city: str
-    street: str
-    zip_code: str
-    house_number: int
-    apartment_number: int
 
 
 class Food(SQLModel, table=True):
@@ -136,35 +161,18 @@ class Food(SQLModel, table=True):
     price: float
 
 
-class Restaurant(SQLModel, table=True):
-    __tablename__ = "restaurant"
+class VehicleType(SQLModel, table=True):
+    __tablename__ = "vehicle_type"
 
     # Keys
-    id_restaurant: int | None = Field(default=None, primary_key=True)
-    id_hours: int | None = Field(default=None, foreign_key="hours.id_hours")
-    id_score: int | None = Field(default=None, foreign_key="score.id_score")
+    id_vehicle_type: int | None = Field(default=None, primary_key=True)
+
+    # Relations
+    vehicles: list["Vehicle"] = Relationship(back_populates="vehicle_type")
 
     # Fields
     name: str
-    pay_cut: float
-    email: str
-    phone: str
-
-
-class Driver(SQLModel, table=True):
-    __tablename__ = "driver"
-
-    # Keys
-    id_driver: int | None = Field(default=None, primary_key=True)
-    id_user: int | None = Field(default=None, foreign_key="user.id_user")
-    id_hours: int | None = Field(default=None, foreign_key="hours.id_hours")
-    id_vehicle: int | None = Field(default=None, foreign_key="vehicle.id_vehicle")
-    id_score: int | None = Field(default=None, foreign_key="score.id_score")
-
-    # Fields
-    date_drivers_license: date
-    date_ID: date
-    is_occupied: bool
+    max_weight: float  # How much it can carry in it's cargo or such
 
 
 class Vehicle(SQLModel, table=True):
@@ -172,22 +180,14 @@ class Vehicle(SQLModel, table=True):
 
     # Keys
     id_vehicle: int | None = Field(default=None, primary_key=True)
-    id_vehicle_type: int | None = Field(default=None, foreign_key="vehicle_type.id_vehicle_type")
+
+    # Relations
+    drivers: list["Driver"] = Relationship(back_populates="vehicle")
+    vehicle_type: VehicleType | None = Relationship(back_populates="vehicles")
 
     # Fields
     brand: str
     model: str
-
-
-class VehicleType(SQLModel, table=True):
-    __tablename__ = "vehicle_type"
-
-    # Keys
-    id_vehicle_type: int | None = Field(default=None, primary_key=True)
-
-    # Fields
-    name: str
-    max_weight: float  # How much it can carry in it's cargo or such
 
 
 class Hours(SQLModel, table=True):
@@ -211,3 +211,22 @@ class Score(SQLModel, table=True):
     # Fields
     amount: int
     sum: float
+
+
+class Driver(SQLModel, table=True):
+    __tablename__ = "driver"
+
+    # Keys
+    id_driver: int | None = Field(default=None, primary_key=True)
+    id_user: int | None = Field(default=None, foreign_key="user.id_user")
+    id_hours: int | None = Field(default=None, foreign_key="hours.id_hours")
+    id_score: int | None = Field(default=None, foreign_key="score.id_score")
+
+    # Relations
+    vehicle: Vehicle | None = Relationship(back_populates="drivers")    
+
+    # Fields
+    date_drivers_license: date
+    date_ID: date
+    is_occupied: bool
+
