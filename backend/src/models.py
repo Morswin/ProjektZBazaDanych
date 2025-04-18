c9f65a4bae4440d19bcaf970befc120fd0806ce3
Morswin <41349347+Morswin@users.noreply.github.com>
2025-04-18 17:24:12 +0200

    Add enums to the models

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,31 +1,60 @@
-import datetime
+from datetime import date, time, datetime
+from enum import Enum
 from sqlmodel import Field, SQLModel
 
-# Those models will be eventually moved to another file, once I get it running
+class TicketType(Enum):
+    COMPLAIN = 1
+    FOOD_NOT_DELIVERED = 2
+    FOOD_IS_COLD = 3
+
+class TicketState(Enum):
+    PENDING = 1
+    RESOLVED = 2
+    REJECTED = 3
 
 class Ticket(SQLModel, table=True):
     id_ticket: int | None = Field(default=None, primary_key=True)
     id_user: int | None = Field(default=None, foreign_key="user.id_user")
-    # id_ticket_type        Enum
-    # id_ticket_state       Enum
+    ticket_type: TicketType
+    ticket_state: TicketState
     description: str
+
+class UserType(Enum):
+    GUEST = 1
+    USER = 2
+    ADMINISTRATOR = 3
+    DRIVER = 4
+
+class Gender(Enum):
+    FEMALE = 1
+    MALE = 2
+    OTHER = 3
+    UNSPECIFIED = 3
 
 class User(SQLModel, table=True):
     id_user: int | None = Field(default=None, primary_key=True)
-    # id_user_type          Enum
+    user_type: UserType
     name: str
     surename: str
     phone: str
     email: str
-    gender: int  # Could be an Enum
+    gender: Gender
     pay: float
+
+class OrderStatus(Enum):
+    SUBMITED = 1
+    COOKING = 2
+    REDY_AT_RESTAURANT = 3
+    IN_DELIVERY = 4
+    DELIVERED = 5
+    CANCELLED = 6
 
 class Order(SQLModel, table=True):
     id_order: int | None = Field(default=None, primaty_key=True)
     id_address: int | None = Field(default=None, foreign_key="address.id_address")
     id_restaurant: int | None = Field(default=None, foreign_key="restaurant.id_restaurant")
     id_ticket: int | None = Field(default=None, foreign_key="ticket.id_ticket")
-    # id_status:            Enum
+    status: OrderStatus
     price: float
     order_date: datetime
     weight: float
@@ -45,7 +74,7 @@ class Food(SQLModel, table=True):
     id_restaurant: int | None = Field(default=None, foreign_key="restaurant.id_restaurant")
     name: str
     description: str
-    # alergens: str ???
+    # alergens: str  # For now we will not implement this. It may be a separate table with a many to many relation.
     price: float
 
 class Restaurant(SQLModel, table=True):
@@ -63,8 +92,8 @@ class Driver(SQLModel, table=True):
     id_hours: int | None = Field(default=None, foreign_key="hours.id_hours")
     id_vehicle: int | None = Field(default=None, foreign_key="vehicle.id_vehicle")
     id_score: int | None = Field(default=None, foreign_key="score.id_score")
-    date_drivers_license: datetime
-    date_ID: datetime
+    date_drivers_license: date
+    date_ID: date
     is_occupied: bool
 
 class Vehicle(SQLModel, table=True):
@@ -81,8 +110,8 @@ class Vehicletype(SQLModel, table=True):
 class Hours(SQLModel, table=True):
     id_hours: int | None = Field(default=None, primary_key=True)
     day: str  # Let's rediscuss if Varchar makes sense here, but for now it stays
-    time_from: datetime
-    time_t: datetime
+    time_from: time
+    time_t: time
 
 class Score(SQLModel, table=True):
     id_score: int | None = Field(default=None, primary_key=True)
