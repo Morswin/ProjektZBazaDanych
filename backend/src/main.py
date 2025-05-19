15eaa60383a219e7a06185d69a17fb5aac75344a
Morswin <41349347+Morswin@users.noreply.github.com>
2025-05-20 01:54:07 +0200

    Dodaj logowanie

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,7 @@
-from models import Restaurant
+from models import Restaurant, User
 from sqlmodel import SQLModel, create_engine, select, Session
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import populate
 
@@ -39,5 +39,14 @@ def list_restaurants(session: Session = Depends(get_session)):
     return session.exec(select(Restaurant)).all()
 
 
-# @app.post()
-# def 
+@app.get("/login", response_model=User)
+def login(email: str, session: Session = Depends(get_session)):
+    user = session.exec(select(User).where(User.email == email)).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@app.get("/users", response_model=list[User])
+def list_users(session: Session = Depends(get_session)):
+    return session.exec(select(User)).all()
